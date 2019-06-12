4ee375731085c11d0e3c5f0120da22ca00db8e39
Hugh Jarvis <hugh.jarvis@blueyonder.co.uk>
2019-06-12 21:37:02 +0100

    first two tests ok, third not ok

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
@@ -1,5 +1,37 @@
 package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.sun.xml.internal.ws.handler.HandlerException;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public class WhiskyRepositoryImpl implements WhiskyRepositoryCustom {
+
+    @Autowired
+    EntityManager entityManager;
+
+    public List<Whisky> getWhiskyByAgebyDistillery(int age, Distillery distillery){
+        List<Whisky> results = null;
+        Session session = entityManager.unwrap(Session.class);
+
+        try {
+            Criteria cr = session.createCriteria(Whisky.class);
+            cr.createAlias("distillery", "distilleryAlias");
+            cr.add(Restrictions.eq("distilleryAlias.name", distillery.getName()));
+            cr.add(Restrictions.eq("age", age));
+            results = cr.list();
+        } catch (HandlerException ex) {
+            ex.printStackTrace();
+        } finally{}
+
+        return results;
+    }
 }
+
